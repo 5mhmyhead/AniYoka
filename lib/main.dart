9bf70eccc953f64886715db2999e1a00dd04e3bb
Dwyane Saludo Sido <disowned.ay@gmail.com>
2026-06-26 17:57:18 +0800

    chore: moved to stacked framework

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:aniyoka/app/app.bottomsheets.dart';
+import 'package:aniyoka/app/app.dialogs.dart';
+import 'package:aniyoka/app/app.locator.dart';
+import 'package:aniyoka/app/app.router.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-void main() {
-  runApp(const AniYokaApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupLocator();
+  setupDialogUi();
+  setupBottomSheetUi();
+  runApp(const MainApp());
 }
 
-class AniYokaApp extends StatelessWidget {
-  const AniYokaApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AniYoka',
-      debugShowCheckedModeBanner: false,
-      home: const Placeholder(),
+      initialRoute: Routes.startupView,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      navigatorKey: StackedService.navigatorKey,
+      navigatorObservers: [StackedService.routeObserver],
     );
   }
 }
